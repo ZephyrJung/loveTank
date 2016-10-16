4274f4bb6271dacd7f3f1ece0f22abb7bb8519cc
O型血橙 <zephyrjung@126.com>
2016-10-16 09:41:24 +0800

    添加旋转前进
    前进未加上左右变量

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 function love.load()	
-	SPEED = 250
+	speed = 10
 	bodyX=250
 	bodyY=250 
 	bodyW=40 
 	bodyH=50
 	centerX=bodyX+bodyW/2
 	centerY=bodyY+bodyH/2
-	Tank={headR=15,headS=6,fireW=5,fireH=40} --headR:坦克脑袋半径，headS:坦克脑袋边数
+	Tank={angle=0,headR=15,headS=6,fireW=5,fireH=40} --headR:坦克脑袋半径，headS:坦克脑袋边数
 	fireX=centerX-Tank.fireW/2
 	fireY=centerY-Tank.fireH-math.cos(math.pi/6)*Tank.headR
 	bullets={}										--The table that contains all bullets.
@@ -21,19 +21,55 @@ function love.draw()
 		love.graphics.circle("fill", v.x, v.y, 4,4)
 	end
 	
+	roateTank(centerX,centerY);
 	--Sets the color to white and draws the "player" and writes instructions.
-	love.graphics.setColor(255, 255, 255)
-	
-	love.graphics.print("Left click to fire towards the mouse.", 50, 50)
+	love.graphics.setColor(255, 255, 255)	
 	love.graphics.rectangle("line", bodyX, bodyY, bodyW, bodyH)
+
+	-- resetAngle();
 	love.graphics.circle("line", centerX, centerY, Tank.headR, Tank.headS)
 	love.graphics.rectangle("line",fireX,fireY,Tank.fireW,Tank.fireH)
+end
 
-	love.graphics.draw(love.graphics.circle("line", 50, 50, 10, 10),x, y,angle)
+function roateTank(posX,posY)
+	love.graphics.translate(posX, posY)--设定初始位置
+	love.graphics.rotate(Tank.angle)--从该位置进行旋转
+	love.graphics.translate(-posX, -posY)--返回初始位置
+end
+
+function resetAngle()
+	love.graphics.translate(0,0)
+end
+
+function moveTank(dir,dt)
+	bodyY=bodyY+dir*dt*speed
+	centerY=centerY+dir*dt*speed
+	fireY=fireY+dir*dt*speed
+end
+
+function keyboardLinstener(dt)
+	if love.keyboard.isDown("a") then
+		Tank.angle = Tank.angle - dt * math.pi/2
+		Tank.angle = Tank.angle % (2*math.pi)
+	elseif love.keyboard.isDown("d") then
+		Tank.angle = Tank.angle + dt * math.pi/2
+		Tank.angle = Tank.angle % (2*math.pi)
+	elseif love.keyboard.isDown("w") then
+		moveTank(-1,dt)
+	elseif love.keyboard.isDown("s") then
+		moveTank(1,dt)
+	elseif love.keyboard.isDown("q") then
+		-- roateTank
+	elseif love.keyboard.isDown("e") then
+		-- roateTank
+	end
 end
 
 function love.update(dt)
-	
+	keyboardLinstener(dt)
+	-- love.timer.sleep(.01)
+	-- Tank.angle = Tank.angle + dt * math.pi/2
+	-- Tank.angle = Tank.angle % (2*math.pi)
 	if love.mouse.isDown(1) then
 		--Sets the starting position of the bullet, this code makes the bullets start in the middle of the player.
 		local startX = fireX+Tank.fireW/2
--- rotate.lua
+++ rotate.lua
@@ -4,12 +4,13 @@ function love.draw()
 	local width = love.graphics.getWidth()
 	local height = love.graphics.getHeight()
 	-- rotate around the center of the screen by angle radians
-	love.graphics.translate(width/2, height/2)
-	love.graphics.rotate(angle)
-	love.graphics.translate(-width/2, -height/2)
+	love.graphics.translate(width/2, height/2)--设定初始位置
+	love.graphics.rotate(angle)--从该位置进行旋转
+	love.graphics.translate(-width/2, -height/2)--返回初始位置
 	-- draw a white rectangle slightly off center
 	love.graphics.setColor(0xff, 0xff, 0xff)
-	love.graphics.rectangle('fill', width/2-100, height/2-100, 300, 400)
+	love.graphics.rectangle('fill', width/2-100, height/2-100, 200, 300)
+
 	-- draw a five-pixel-wide blue point at the center
 	love.graphics.setPointSize(5)
 	love.graphics.setColor(0, 0, 0xff)
